878b512c685a1c66427e5dd4d454408fba91047c
jeonhaelee <jhealee0624@gmail.com>
2022-03-08 23:39:18 +0900

    문자열을 정수로 바꾸기

---
+++ src/Practice_1/prac6.java
@@ -0,0 +1,22 @@
+package Practice_1;
+
+class Prac6 {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = s.split("");
+        String wd = "";
+        if(arr[0]=="-") {
+        	wd = "-";
+        	for(int i = 1; i<arr.length; i++) {
+        		wd = wd + arr[i];
+        	}
+        	answer = Integer.parseInt(wd);
+        } else {
+        	for(int i = 0; i<arr.length; i++) {
+        		wd = wd + arr[i];
+        	}
+        	answer = Integer.parseInt(wd);
+        }
+        return answer;
+    }
+}
