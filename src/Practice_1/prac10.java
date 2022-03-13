53c154b2943d0cf033cdd2dc5a2ec940788f801b
jeonhaelee <jhealee0624@gmail.com>
2022-03-13 21:15:41 +0900

    서울에서 김서방 찾기

---
+++ src/Practice_1/prac10.java
@@ -0,0 +1,15 @@
+package Practice_1;
+
+public class prac10 {
+	public String solution(String[] seoul) {
+		String answer = "김서방은 ";
+		for (int i = 0; i < seoul.length; i++) {
+			if (seoul[i].equals("Kim")) {
+				answer += Integer.toString(i);
+				answer += "에 있다";
+			}
+		}
+		System.out.println(answer);
+		return answer;
+	}
+}
